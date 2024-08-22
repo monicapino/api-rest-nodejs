2abe89b384d22bda0489e75de495c1f6a72303d7
Monica Pino <112265101+monicapino@users.noreply.github.com>
2024-08-22 13:32:27 -0300

    chore: Production config

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -2,9 +2,12 @@
 import { app } from './app'
 import { env } from '../env'
 
-app.listen({
+app
+  .listen({
     port: env.PORT,
-})
-.then(() => {
-    console.log('HTTP Server Running')
-})  
+    //Código que restava para funcionar.
+    host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
